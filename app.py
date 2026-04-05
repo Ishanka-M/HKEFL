746cc5b1edf7137a9e6b5dacc892a7362efcfce5
Ishanka Madusanka <66905286+Ishanka-M@users.noreply.github.com>
2026-04-06 00:36:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -747,8 +747,8 @@ def process_picking(inv_df, req_df, batch_id, inv_original=None):
                             'Invoice Number':     invoice_number_val,
                             'Grn Number':         grn_number_val,
                         })
-                    else:
-                        pick_rows[-1]['Gen Pallet ID'] = ''
+            else:
+                pick_rows[-1]['Gen Pallet ID'] = ''
 
                     temp_inv.at[idx, actual_qty_col] -= take
                     needed     -= take
